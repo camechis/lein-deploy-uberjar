011d9f9f1c6bfb019749386b3c76eae178db0fde
Travis Camechis <camechis@gmail.com>
2015-08-17 22:12:29 -0400

    prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camechis/deploy-uberjar "0.2.0"
+(defproject camechis/deploy-uberjar "0.3.0"
   :description "basic lein plugin to deploy an uberjar"
   :url "https://github.com/camechis/lein-deploy-uberjar"
   :license {:name "Apache License 2.0"
